b53e4e18fee1af518d310a39ad933caafb1056fa
Arman Hossain <web.devarman@gmail.com>
2025-11-01 04:08:22 +0600

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+import express from "express"
+import {ENV} from "./lib/env.js"
+
+const app = express ()
+
+app.get("/", (req, res)=>{
+    res.status(200).json({msg:"success from backend on port 485"})
+})
+
+app.listen(ENV.PORT, ()=> console.log("server is running on port", ENV.PORT))
